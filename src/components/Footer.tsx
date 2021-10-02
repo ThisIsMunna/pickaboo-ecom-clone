168085b76010ba9f632c4efe8e4bfdb6635b5166
ThisIsMunna <authenticmunna@gmail.com>
2021-10-02 06:23:06 +0600

    added footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,34 @@
 import React from "react";
-
+import playStoreLogo from "./../images/getItOnPlayStore.png"
+import appStoreLogo from "./../images/getItOnAppStore.png"
 import "./Footer.css";
 
-const FooterOptions = () => {
+interface Props{
+    num: number;
+}
+const footerData = [
+    {
+        title: "ABOUT",
+        options: ["About Us", "Privacy Policy", "Cookie Policy", "Why Shop With Us", "Terms & Conditions", "Help"]
+    },
+    {
+        title: "CONTACT",
+        options: ["Contact Us"]
+    },
+    {
+        title: "PICKABOO",
+        options: ["House 10, Road 12", "Block F, Niketan, Gulshan 1,",  "Dhaka - 1212, Bangladesh", "+8809666745745"]
+    }
+]
+
+const FooterOptions = ({num}: Props) => {
   return (
-    <div className="footerOptionsWrapper textCenter">
-      <div className="footerOptionsTitle">ABOUT</div>
+    <div className={`footerOptionsWrapper ${footerData[num].title === "ABOUT" || footerData[num].title === "CONTACT"? "textCenter": ""}`}>
+      <div className="footerOptionsTitle">{footerData[num].title}</div>
       <div className="footerOptions ">
-        <a>About Us</a>
-        <a>Privacy Policy</a>
-        <a href="">Cookie Policy</a>
-        <a href="">Why Shop With Us</a>
-        <a href="">Terms {`&`} Conditions</a>
-        <a href="">Help</a>
+          {footerData[num].options.map(option =>( 
+              <a>{option}</a>
+          ))}
       </div>
     </div>
   );
@@ -22,7 +38,17 @@ const Footer = () => {
   return (
     <div className="footerWrapper">
       <div className="footer">
-          <FooterOptions/>
+          {footerData.map((obj, idx) => ( 
+          <FooterOptions num={idx}/>
+  ))}
+
+    <div className="footerOptionsWrapper">
+      <div className="footerOptionsTitle">DOWNLOAD OUR APP</div>
+      <div className="downloadOptions">
+        <img className="logoImg" src={playStoreLogo} alt="" />
+        <img className="logoImg" src={appStoreLogo} alt="" />
+      </div>
+    </div>
       </div>
     </div>
   );
